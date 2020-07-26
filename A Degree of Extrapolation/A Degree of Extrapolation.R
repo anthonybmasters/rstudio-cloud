9611cbf9436e9c7d3eb9f5fddc882610e3b184e9
anthonybmasters <59419178+anthonybmasters@users.noreply.github.com>
2020-07-26 07:57:19 +0100

    Add files via upload

--- A Degree of Extrapolation/A Degree of Extrapolation.R
+++ A Degree of Extrapolation/A Degree of Extrapolation.R
@@ -1,10 +1,8 @@
-#Load packages
+## Packages and Themes
 library(tidyverse)
 library(readxl)
 library(forecast)
 
-
-#Set themes
 theme_clean <- theme_bw(base_family="Calibri") + 
   theme(legend.position = "top",
         legend.title = element_text(size = 12),
@@ -15,23 +13,26 @@ theme_clean <- theme_bw(base_family="Calibri") +
         panel.grid.major = element_blank(),
         panel.grid.minor = element_blank(),
         panel.border = element_blank())
+
 theme_set(theme_clean)
 
-#Import and tidy the data
-hesa_degree_df <- read_excel("HESA Class of Degree by First Degree Graduates - 2019-12-30.xlsx",
+## Import and tidy the data
+hesa_degree_df <- read_excel("A Degree of Extrapolation/HESA Class of Degree by First Degree Graduates - 2019-12-30.xlsx",
                              sheet = "DATA") %>%
   dplyr::rename(ThirdPass = "Third/Pass") %>%
   dplyr::mutate(Classified = First + Upper_Second + Lower_Second + ThirdPass,
                 First_Share = (First / Classified)* 100)
+
 hesa_class_df <- hesa_degree_df %>%
   select(1:5) %>%
   pivot_longer(cols = 2:5,
                names_to = "Class",
                values_to = "Count")
+
 hesa_class_df$Class <- factor(hesa_class_df$Class,
                               levels = c("First", "Upper_Second", "Lower_Second", "ThirdPass"))
 
-#Draw the graphs
+## Draw the graphs
 hesa_class_gg <- ggplot(data = hesa_class_df,
                         aes(x = AcademicYear_EY,
                             y = Count,
@@ -46,6 +47,7 @@ hesa_class_gg <- ggplot(data = hesa_class_df,
        caption = "Source: HESA Enrolment and Qualification reports; HESA Higher Education Student Statistics.",
        x = "Academic Year (End Year)",
        y = "")
+
 hesa_firstshare_gg <- ggplot(data = hesa_degree_df,
                              aes(x = AcademicYear_EY,
                                  y = First_Share)) +
@@ -59,14 +61,18 @@ hesa_firstshare_gg <- ggplot(data = hesa_degree_df,
        x = "Academic Year (End Year)",
        y = "")
 
-#Forecasting
+## Forecasting
 hesa_firstshare_df <- hesa_degree_df %>%
-  select("AcademicYear_EY", "First_Share")
+  dplyr::select("AcademicYear_EY", "First_Share")
+
 hesa_firstshare_ts <- ts(data = hesa_firstshare_df$First_Share,
                          start = 2004,
                          frequency = 1)
+
 forecast(hesa_firstshare_ts, h = 12)
+
 forecast(hesa_firstshare_ts, h = 12)$model
+
 hesa_tspred_gg <- forecast(hesa_firstshare_ts, h = 12) %>%
   autoplot() +
   labs(title = "These forecasts have wide prediction intervals by 2029/30",
@@ -74,3 +80,32 @@ hesa_tspred_gg <- forecast(hesa_firstshare_ts, h = 12) %>%
        caption = "Sources: HESA Higher Education Statistics; Forecast function in R.",
        x = "Academic Year (End Year)", y = "") +
   ylim(c(0,100))
+
+## Forecasting - Logit Transformation
+hesa_fslogit_df <- hesa_degree_df %>%
+  dplyr::mutate(First_Share_Logit = log(First_Share/(100-First_Share))) %>%
+  dplyr::select("AcademicYear_EY", "First_Share_Logit")
+
+hesa_fslogit_ts <- ts(data = hesa_fslogit_df$First_Share_Logit,
+                         start = 2004,
+                         frequency = 1)
+
+hesa_fslogit_fc <- forecast(hesa_fslogit_ts, h = 12)
+
+hesa_fslogit_fc$model
+
+hesa_fslogit_fc$mean <- 100*exp(hesa_fslogit_fc$mean)/(1+exp(hesa_fslogit_fc$mean))
+hesa_fslogit_fc$lower <- 100*exp(hesa_fslogit_fc$lower)/(1+exp(hesa_fslogit_fc$lower))
+hesa_fslogit_fc$upper <- 100*exp(hesa_fslogit_fc$upper)/(1+exp(hesa_fslogit_fc$upper))
+hesa_fslogit_fc$x <- 100*exp(hesa_fslogit_fc$x)/(1+exp(hesa_fslogit_fc$x))
+
+hesa_fslogit_fc
+
+hesa_fslogit_gg <- hesa_fslogit_fc %>%
+  autoplot() +
+  labs(title = "For 2029/30, the central forecast is 58% (with the 80% prediction interval from 45% to 70%).",
+       subtitle = "ETS(A,A,N) forecast of the logit-transformed Firsts proportion achieved by first-time graduates at UK institutions [%] (80% and 95% prediction intervals shown).",
+       caption = "Sources: HESA Higher Education Statistics; Forecast function in R.",
+       x = "Academic Year (End Year)", y = "") +
+  scale_y_continuous(limits = c(0,100),
+                     expand = c(0,0))
